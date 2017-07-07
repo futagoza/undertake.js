a64348bc27b8967c50dd2b7bcf063b0ea4d17f6d
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2017-07-07 03:55:17 +0100

    updated licence banner

---
+++ src/banner.js
@@ -0,0 +1,5 @@
+/*
+    undertake.js, https://github.com/futagoza/undertake.js
+    Copyright © 2017 Futago-za Ryuu, https://futagoza.github.io/
+    Released under the MIT License, http://opensource.org/licenses/MIT
+*/
